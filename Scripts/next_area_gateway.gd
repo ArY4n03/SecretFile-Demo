57620dab6c901273348a2180f557bc45c51942c3
Aryan Soy <aryansoy3@gmail.com>
2025-01-12 22:52:00 +0530

    added next area gateway

--- Scripts/next_area_gateway.gd
+++ Scripts/next_area_gateway.gd
@@ -1,12 +1,7 @@
 extends Area2D
 
 @export var next_area : PackedScene
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if $ProgressBar.value < 100:
 		
@@ -23,5 +18,4 @@ func _process(delta: float) -> void:
 			
 func update_progress(delta):
 	$ProgressBar.visible = true
-	$ProgressBar.value +=10 * delta
-	print($ProgressBar.value)
+	$ProgressBar.value += 20 * delta
